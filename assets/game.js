40fc6f46b3d4452f9493debfb9bd58bc919494be
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-01-22 20:17:44 +0800

    文案修改

--- assets/game.js
+++ assets/game.js
@@ -1,8 +1,14 @@
 function invalidInputCout() {
-    console.log("[Warn]: invalid input;");
+    if(language == "english") console.log("[Warn]: invalid input;");
+    else if(language == "中文") console.log("[Warn]: 无效的输入");
 }
 function unSetInputCout() {
-    console.log("[Error]: unset input;");
+    if(language == "english") console.log("[Error]: unset input;");
+    else if(language == "中文") console.log("[Error]: 未设置输入;");
+}
+function needDebugOnCout() {
+    if(language == "english") console.log("[Error]: ignored, needs \"debugMode\" on(true);");
+    else if(language == "中文") console.log("[Error]: 忽略, 需要 \"debugMode\" 为正(true);");
 }
 // 定义
 const canvas = document.getElementById("canvas");
@@ -37,28 +43,36 @@ function debug(option) {
         unSetInputCout();
     }
 }
+// 控制台输出
 function variableStatus() {
     if(debugMode) {
-        console.log("localStorage.getItem(\"language\") = " + localStorage.getItem("language")); 
-    } else {
+        if(language == "english") console.group("variable values / information:");
+        else if(language == "中文") console.group("变量值 / 信息:");
+        
+        console.log("localStorage.getItem(\"language\") = " + localStorage.getItem("language") + "\n debugMode: " + debugMode + "\n");
 
+        if(language == "english") console.group("Note: only for staric information;");
+        else if(language == "中文") console.group("注意: 仅适用于静态信息");
+    } else {
+        needDebugOnCout();
     }
 }
 function note() {
-    if(language == "english")  console.log("Notes: \n to change console output, please change variable \"language\" s value, \n such as \"changeLanguage(\"中文\");\", until lastest edition(" + lastestEdition + "), \n available languages: \"english\" and \"中文\";");
-    else if(language == "中文") console.log("帮助: \n 如果你要改变控制台输出的语言的话, 请改变 \"language\" 的值, 比如 \"changeLanguage(\"english\");\", 截止最后的版本(" + lastestEdition + "), 可用的语言为: \"english\" 和 \"中文\";");
+    if(language == "english")  console.log("Notes:\nto change console output, please change variable \"language\" s value,\nsuch as \"changeLanguage(\"中文\");\", edition(" + lastestEdition + "), \n available languages: \"english\" and \"中文\";");
+    else if(language == "中文") console.log("帮助:\n如果你要改变控制台输出的语言的话, 请改变 \"language\" 的值,\n比如 \"changeLanguage(\"english\");\", 版本(" + lastestEdition + "), 可用的语言为: \"english\" 和 \"中文\";");
 }
 function changeLanguage(option) {
     language = option;
     localStorage.setItem("language", option);
-    if(language == "endlish") console.log("successful change the language, language: " + language + "")
+    if(language == "endlish") console.log("successful change the language, language: " + language);
+   else  if(language == "中文") console.log("成功的更改了语言: " + language);
 }
 
 // 监听事件
 window.onresize = () => {
     reSize();
-    if(language == "endlish") console.log("detect window s size changed: \n windowHeight: " + windowHeight + " windowWidth: " + windowWidth + " (-5)");
-    else if(language == "中文") console.log("检测到窗口大小变化: \n windowHeight: " + windowHeight + " windowWidth: " + windowWidth + " (-5)");
+    if(language == "endlish") console.log("detect window s size changed:\nwindowHeight: " + windowHeight + " windowWidth: " + windowWidth + " (-5)");
+    else if(language == "中文") console.log("检测到窗口大小变化:\nwindowHeight: " + windowHeight + " windowWidth: " + windowWidth + " (-5)");
 };
 window.onmousemove = (event) => {
     mouseX = event.pageX;
@@ -93,7 +107,8 @@ function render() {
 }
 
 // 初始化
-console.log("Author: NikeMa2011, lastest edition: " + lastestEdition + "\n github profile: https://github.com/NikeMa2011");
+if(language == "english") console.log("Author: NikeMa2011, edition: " + lastestEdition + "\ngithub profile: https://github.com/NikeMa2011");
+else if(language == "中文") console.log("作者: NikeMa2011, 版本: " + lastestEdition + "\ngithub链接 : https://github.com/NikeMa2011/RPG");
 if(localStorage.getItem("language") == undefined) {
     console.log("Since you have not set or open this website before(localStorage.getItem(\"language\") is \"" + localStorage.getItem("language") +"\"), please set the language,");
     localStorage.setItem("language", "english");
