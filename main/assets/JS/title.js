9ce09f2e888d5f4f6126c0e23c59ebdce76b7509
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-03-14 11:40:39 +0800

    title.js 格式化

--- main/assets/JS/title.js
+++ main/assets/JS/title.js
@@ -1,9 +1,9 @@
 function websiteTitleChange() {
-    if(language == "english") {
+    if (language == "english") {
         Cout("Author: NikeMa2011, edition: " + lastestEdition + "\ngithub profile: https://github.com/NikeMa2011");
         titleTag.innerHTML = ("RPGgame - githubEdition");
     }
-    else if(language == "中文") {
+    else if (language == "中文") {
         Cout("作者: NikeMa2011, 版本: " + lastestEdition + "\ngithub链接 : https://github.com/NikeMa2011/RPG");
         titleTag.innerHTML = ("RPG小游戏 - github版本");
     }
