70225b5638a6dabab462a427c3fb561b4990ad55
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-03-18 07:59:23 +0800

    更改title.js /JS的语句引用

--- main/assets/JS/title.js
+++ main/assets/JS/title.js
@@ -1,10 +1,10 @@
 function websiteTitleChange() {
     if (language == "english") {
-        Cout("Author: NikeMa2011, edition: " + lastestEdition + "\ngithub profile: https://github.com/NikeMa2011");
-        titleTag.innerHTML = ("RPGgame - githubEdition");
+        Cout("Author" + text.author + "edition: " + lastestEdition + "\ngithub profile: " + text.githubLink);
+        title.innerHTML = (title.english);
     }
     else if (language == "中文") {
-        Cout("作者: NikeMa2011, 版本: " + lastestEdition + "\ngithub链接 : https://github.com/NikeMa2011/RPG");
-        titleTag.innerHTML = ("RPG小游戏 - github版本");
+        Cout("作者" + text.author + "版本: " + lastestEdition + "\ngithub链接 : " + text.githubLink);
+        title.innerHTML = (title.english);
     }
 }
