ff2b4b200e54519e5cacbde4430e8075cd88b34a
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-03-18 08:47:25 +0800

    title.js /JS输出语句添加空格

--- main/assets/JS/title.js
+++ main/assets/JS/title.js
@@ -1,10 +1,10 @@
 function websiteTitleChange() {
     if (language == "english") {
-        Cout("Author" + text.author + "edition: " + lastestEdition + "\ngithub profile: " + text.githubLink);
+        Cout("Author: " + text.author + " edition: " + lastestEdition + "\ngithub profile: " + text.githubLink);
         title.innerHTML = (title.english);
     }
     else if (language == "中文") {
-        Cout("作者" + text.author + "版本: " + lastestEdition + "\ngithub链接 : " + text.githubLink);
+        Cout("作者: " + text.author + " 版本: " + lastestEdition + "\ngithub链接 : " + text.githubLink);
         title.innerHTML = (title.english);
     }
 }
