71642c9f359e930226b9c2c75eb02a852768c183
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-09-29 18:34:47 +0800

    在itemClass.js添加人物类

---
+++ assets/JS/class/itemClass.js
@@ -0,0 +1,29 @@
+class people {
+    constructor(x, y, z, IDcode, health, maximumHealth, weight, maximumWeight, dead) {
+        this.x = x;
+        this.y = y;
+        this.IDcode = IDcode;
+        this.health = health;
+        this.maximumHealth = maximumHealth;
+        this.weight = weight;
+        this.maximumWeight = maximumWeight;
+        this.dead = dead;
+    }
+
+    hurt(damage) {
+        let healthAfterHurt = health - damage;
+
+        if (healthAfterHurt > 0) {
+            return healthAfterHurt;
+        } else {
+            health = 0;
+            dead = true;
+        }
+    }
+
+    setUp() {
+        health = maximumHealth;
+        weight = maximumWeight;
+        dead = false;
+    }
+}
--- assets/JS/main.js
+++ assets/JS/main.js
@@ -5,3 +5,7 @@ let windowHeight, windowWidth;
 
 let framePerSecond = 60;
 let framePerSecondInMillisecond;
+
+let player = new people(0, 0, 0, 0, null, 460, null, 700000, null)
+
+player.setUp();
